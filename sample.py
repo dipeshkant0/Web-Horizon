3a343e36fb3137a659c170f4e91c0c55d6fa42ab
Vishalkm206 <vishalkm206@gmail.com>
2022-10-17 05:18:49 +0530

    Hello

---
+++ sample.py
@@ -0,0 +1,11 @@
+
+# import counter class from collections module
+from collections import Counter
+ 
+# Creation of a Counter Class object using
+# string as an iterable data container
+x = Counter("geeksforgeeks")
+ 
+# printing the elements of counter object
+for i in x.elements():
+    print (i)
