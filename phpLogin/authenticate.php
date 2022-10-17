e4e223cf385289b624e8325635f71273d18d1e8e
dipeshkant0 <dipeshkant0@gmail.com>
2022-10-17 14:36:56 +0530

    add updated file

--- phpLogin/authenticate.php
+++ phpLogin/authenticate.php
@@ -35,7 +35,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
             $_SESSION['loggedin'] = TRUE;
             $_SESSION['name'] = $_POST['username'];
             $_SESSION['id'] = $id;
-            header('Locatio:../Home.html');
+            header('Location: home.php');
         } else {
             // Incorrect password
             echo 'Incorrect username and/or password!';
